b978db2951ab4c6f7d0dbc3b3d6f1350f8265a88
o_jun <vhzkclq0705@naver.com>
2022-06-26 22:48:41 +0900

    [MOD] Add button animation

--- MyTimer/UI/ButtonAnimation.swift
+++ MyTimer/UI/ButtonAnimation.swift
@@ -11,52 +11,55 @@ import UIKit
 extension TimerListVC {
     // AddButton tap animation
     func displayButtons(_ show: Bool) {
+        animate(show: show, duration: 0.05)
+    }
+    
+    func changeView(_ show: Bool) {
         tableView.isUserInteractionEnabled = !show
         tableView.layer.opacity = show ? 0.7 : 1
         controlView.isHidden = !show
-        
-        let angle: CGFloat = show ? Double.pi / 4 : 0
+    }
+    
+    func animate(show: Bool, duration: TimeInterval) {
+        if show { self.changeView(show) }
         UIView.animate(
-            withDuration: 0.1,
-            delay: 0,
-            options: .transitionCrossDissolve,
-            animations: {
-                self.addButton.transform = CGAffineTransform(rotationAngle: angle)
-                if show {
-                    self.addTimerButton.alpha = 1
-                    self.addTimerLabel.alpha = 1
-                } else {
-                    self.settingsButton.alpha = 0
-                    self.settingsLabel.alpha = 0
-                }
-            }, completion: { _ in
+            withDuration: duration,
+            animations: { self.firstAnimation(show) }) { _ in
                 UIView.animate(
-                    withDuration: 0.1,
-                    delay: 0,
-                    options: .transitionCrossDissolve,
-                    animations: {
-                        if show {
-                            self.addSectionButton.alpha = 1
-                            self.addSectionLabel.alpha = 1
-                        } else {
-                            self.addTimerButton.alpha = 0
-                            self.addTimerLabel.alpha = 0
-                        }
-                    }, completion: { _ in
+                    withDuration: duration,
+                    animations: { self.secondAnimation(show) }) { _ in
                         UIView.animate(
-                            withDuration: 0.1,
-                            delay: 0,
-                            options: .transitionCrossDissolve,
-                            animations: {
-                                if show {
-                                    self.settingsButton.alpha = 1
-                                    self.settingsLabel.alpha = 1
-                                } else {
-                                    self.addSectionButton.alpha = 0
-                                    self.addSectionLabel.alpha = 0
-                                }
-                            })
-                    })
-            })
+                            withDuration: duration,
+                            animations: { self.thirdAnimation(show) }) { _ in
+                                if !show { self.changeView(show) }
+                            }
+                    }
+            }
+    }
+    
+    func firstAnimation(_ show: Bool) {
+        let angle: CGFloat
+        
+        if show {
+            [addTimerButton, addTimerLabel].forEach { $0.alpha = 1 }
+            angle = Double.pi / 4
+        }
+        else {
+            [settingsButton, settingsLabel].forEach { $0.alpha = 0 }
+            angle = 0
+        }
+        
+        addButton.transform = CGAffineTransform(rotationAngle: angle)
+    }
+    
+    func secondAnimation(_ show: Bool) {
+        [addSectionButton, addSectionLabel]
+            .forEach { $0.alpha = show ? 1 : 0 }
+    }
+    
+    func thirdAnimation(_ show: Bool) {
+        show
+        ? [settingsButton, settingsLabel].forEach { $0.alpha = 1 }
+        : [addTimerButton, addTimerLabel].forEach { $0.alpha = 0 }
     }
 }
