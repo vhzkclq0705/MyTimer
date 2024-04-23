66c44a8f921dd73984a4db83bc8d68fceb520db8
oh_jun <vhzkclq0705@naver.com>
2024-04-23 21:27:22 +0900

    [ADD] gitignore

--- MyTimer/Cell/TimerListHeaderCell.swift
+++ MyTimer/Cell/TimerListHeaderCell.swift
@@ -117,3 +117,18 @@ extension TimerListHeaderCell {
         clearView.setContentHuggingPriority(.defaultLow, for: .horizontal)
     }
 }
+
+extension UITableViewCell {
+    
+    func showSeparator() {
+        DispatchQueue.main.async {
+            self.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        }
+    }
+    
+    func hideSeparator() {
+        DispatchQueue.main.async {
+            self.separatorInset = UIEdgeInsets(top: 0, left: self.bounds.size.width, bottom: 0, right: 0)
+        }
+    }
+}
--- Pods/Target Support Files/ExpyTableView/ExpyTableView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ExpyTableView : NSObject
-@end
-@implementation PodsDummy_ExpyTableView
-@end
